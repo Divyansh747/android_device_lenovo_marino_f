b82678273cf738e03da6900c53a9f447a095c7d5
divyansh747 <divyansh.1999.dr@gmail.com>
2018-03-23 17:07:42 -0400

    initial push

---
+++ product/ramdisk.mk
@@ -0,0 +1,20 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
+    $(LOCAL_PATH)/rootdir/factory_init.project.rc:root/factory_init.project.rc \
+    $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
+    $(LOCAL_PATH)/rootdir/fstab.charger:root/fstab.charger \
+    $(LOCAL_PATH)/rootdir/fstab.mt6757:root/fstab.mt6757 \
+    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
+	$(LOCAL_PATH)/rootdir/init.microtrust.rc:root/init.microtrust.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6757.rc:root/init.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.mt6757.usb.rc:root/init.mt6757.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
+    $(LOCAL_PATH)/rootdir/init.rc:root/init.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.spectrum.rc:root/init.spectrum.rc \
+    $(LOCAL_PATH)/rootdir/init.spectrum.sh:root/init.spectrum.sh \
+    $(LOCAL_PATH)/rootdir/meta_init.modem.rc:root/meta_init.modem.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.project.rc:root/meta_init.project.rc \
+    $(LOCAL_PATH)/rootdir/meta_init.rc:root/meta_init.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.mt6757.rc:root/ueventd.mt6757.rc \
+    $(LOCAL_PATH)/rootdir/init.recovery.mt6757.rc:root/init.recovery.mt6757.rc
