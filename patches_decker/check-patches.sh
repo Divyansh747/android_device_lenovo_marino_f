37472d7c685f51bf0dc73ea3a22579592f776831
divyansh747 <divyansh.1999.dr@gmail.com>
2018-04-11 16:34:38 +0530

    fix typo

--- patches_decker/check-patches.sh
+++ patches_decker/check-patches.sh
@@ -2,13 +2,13 @@
 clear
 cd ../../../..
 cd frameworks/av
-git apply -v --check ../../device/blu/p6601/patches_decker/0001-mtk-audio-fix.patch
-git apply -v --check ../../device/blu/p6601/patches_decker/0002-fix-access-wvm-to-ReadOptions.patch
-git apply -v --check ../../device/blu/p6601/patches_decker/0003-add-mising-MediaBufferGroup-acquire_buffer-symbol.patch
-git apply -v --check ../../device/blu/p6601/patches_decker/0004-add-mising-MediaBufferGroup-acquire_buffer-symbol-2.patch
+git apply -v --check ../../device/lenovo/marino_f/patches_decker/0001-mtk-audio-fix.patch
+git apply -v --check ../../device/lenovo/marino_f/patches_decker/0002-fix-access-wvm-to-ReadOptions.patch
+git apply -v --check ../../device/lenovo/marino_f/patches_decker/0003-add-mising-MediaBufferGroup-acquire_buffer-symbol.patch
+git apply -v --check ../../device/lenovo/marino_f/patches_decker/0004-add-mising-MediaBufferGroup-acquire_buffer-symbol-2.patch
 cd ../..
 cd frameworks/native
-git apply -v  ../../device/blu/p6601/patches_decker/0005-_ZN7android13GraphicBufferC1Ejjij-symbol-fix-on-fram.patch
+git apply -v  ../../device/lenovo/marino_f/patches_decker/0005-_ZN7android13GraphicBufferC1Ejjij-symbol-fix-on-fram.patch
 #echo "Проверьте, все ли патчи из каталога прописаны в скрипте!"
 cd ../..
 #echo Patches Applied Successfully!
