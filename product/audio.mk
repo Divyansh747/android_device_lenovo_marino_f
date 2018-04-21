97b245289c8688fd9ef24fa9bd44ea3e3c678298
divyansh747 <divyansh.1999.dr@gmail.com>
2018-04-21 14:23:06 +0530

    update product files

--- product/audio.mk
+++ product/audio.mk
@@ -5,6 +5,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio_policy.default \
     libaudio-resampler \
+	libtinyalsa \
     libaudiopolicymanagerdefault \
     libmtk_symbols \
     libtinycompress \
--- product/ril.mk
+++ product/ril.mk
@@ -1,5 +1,18 @@
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.kernel.android.checkjni=0 \
-	ro.telephony.ril_class=MT6757 \
-	ro.telephony.ril.config=fakeiccid
+    ro.kernel.android.checkjni=0 \
+    ro.com.android.mobiledata=false
+
+# RIL
+PRODUCT_PACKAGES += \
+    libccci_util \
+    librilmtk \
+    mtkrild
+
+
+# Configurations
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml
+
+PRODUCT_PACKAGES += \
+Stk
